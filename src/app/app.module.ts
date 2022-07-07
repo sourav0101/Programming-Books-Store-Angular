2ce45566ede2ab94cffe088c8699ff838fd03e7e
sourav0101 <souravdas9690@gmail.com>
2022-07-07 21:18:17 +0600

    Event Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
-import { SouravDasComponent } from './sourav-das/sourav-das.component';
+//import { SouravDasComponent } from './sourav-das/sourav-das.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    SouravDasComponent
+    //SouravDasComponent
   ],
   imports: [
     BrowserModule,
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  name:string= 'Introduction to Algorithms'; 
+  author:string  ='Ronald L. Rivest'; 
+  src:string = 'https://m.media-amazon.com/images/I/41SNoh5ZhOL._AC_SY1000_.jpg';
+
+  name2:string= 'Competitive Programming 4'; 
+  author2:string  ='Felix Halim'; 
+  src2:string = 'https://images.booksense.com/images/515/745/9781716745515.jpg';
+
+  isDisabled:boolean = false; 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  //function... 
+  handleClick(){
+     //alert("clicked");
+     //this.isDisabled = true; 
+
+
   }
 
 }
