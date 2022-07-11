cd89b8072837d0f531acdd4e600325c5f01c690f
sourav0101 <souravdas9690@gmail.com>
2022-07-11 17:12:47 +0600

    :lock: Angular Login Form

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginForm } from 'src/app/types/Auth';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email:string = '';
+  form: LoginForm = {
+    email: '',
+    password: '', 
+  };
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  submit()
+  {
+    console.log(this.form); //form e ja thakebe. 
   }
 
 }
